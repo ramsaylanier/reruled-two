4817f0713f4d2f061b805c102d8363d92bdda79d
Ramsay Lanier <rlanier@NovettaAdmins-MacBook-Pro-2.local>
2016-10-03 13:05:39 -0400

    lint fixes for app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default class App extends React.Component{
-  render(){
-    return(
+export default class App extends React.Component {
+  render () {
+    return (
       <h1>This is the app Component</h1>
     )
   }
