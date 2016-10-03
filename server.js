d346b0bc73c2139afc6cfd87724698999f242b24
Ramsay Lanier <rlanier@NovettaAdmins-MacBook-Pro-2.local>
2016-10-03 13:04:36 -0400

    update webpack config and server to use express

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const express = require('express')
+const path = require('path')
+const webpack = require('webpack')
+const webpackMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const config = require('./webpack.config')
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+const isDev = process.env.NODE_ENV !== 'production'
+const port = isDev ? 3000 : process.env.PORT
+const app = express()
+
+if (isDev) {
+  const compiler = webpack(config)
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: true,
+      timings: true,
+      chunks: true,
+      chunkModules: true,
+      modules: true
+    }
+  })
+
+  app.use(middleware)
+  app.use(webpackHotMiddleware(compiler))
+  app.get('*', function response (req, res) {
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')))
+  })
+} else {
+  app.use(express.static(path.join(__dirname, '/dist')))
+  app.get('*', function response (req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+  })
+}
+
+app.listen(port, '0.0.0.0', function onStart (err) {
   if (err) {
-    return console.log(err);
+    console.log(err)
   }
-
-  console.log('Listening at http://localhost:3000/');
-});
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,42 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'eval-source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index.js'
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'src/index.js')
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
   ],
   module: {
     loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.json?$/,
+      loader: 'json'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
     }]
   }
-};
+}
