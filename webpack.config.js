f77bc21e7e2276f3de23dcd831a5315333bab3f0
Ramsay Lanier <rlanier@NovettaAdmins-MacBook-Pro-2.local>
2016-10-04 14:16:41 -0400

    update webpack config - fix HMR

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   devtool: 'eval-source-map',
   entry: [
-    'webpack-hot-middleware/client?reload=true',
+    'react-hot-loader/patch',
+    'webpack-hot-middleware/client',
     path.join(__dirname, 'src/index.js')
   ],
   output: {
@@ -27,16 +28,29 @@ module.exports = {
     })
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.json?$/,
-      loader: 'json'
-    }, {
-      test: /\.css$/,
-      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
-    }]
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }, {
+        test: /\.json?$/,
+        loader: 'json'
+      }, {
+        test: /\.css$/,
+        loaders: [
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+        ]
+      }, {
+        test: /\.scss$/,
+        loaders: [
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'resolve-url',
+          'sass?sourceMap'
+        ]
+      }
+    ]
   }
 }
