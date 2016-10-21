f578e81157e656622785e30b71b5de6aad2c0f40
Ramsay Lanier <rlanier@NovettaminsMBP2.fios-router.home>
2016-10-21 10:52:17 -0400

    add logout routing function

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {ApolloProvider} from 'react-apollo'
 import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import Homepage from './components/layout/pages/homepage'
-import Login from './components/login'
+import Login from './components/layout/pages/login'
 import GameSingle from './components/games/gameSingle'
 import UserProfile from './components/profile/UserProfile'
 import Layout from './components/layout/default'
@@ -14,11 +14,24 @@ function checkForUserSession (nextState, replace, callback) {
   return authApi.checkSession().then(res => {
     if (res.data.loggedIn) {
       const user = res.data.user
-      console.log('user: ', user)
       store.dispatch({type: 'LOG_IN', user: user})
     }
 
     callback()
+  })
+}
+
+function logOut (nextState, replace, cb) {
+  return authApi.logout().then(res => {
+    store.dispatch({type: 'LOG_OUT'})
+    replace({
+      pathname: '/login',
+      state: {
+        nextPathname: nextState.location.pathname
+      }
+    })
+
+    cb()
   })
 }
 
@@ -27,6 +40,7 @@ const App = (props) => {
   return (
     <ApolloProvider client={client} store={store}>
       <Router history={history}>
+        <Route path="logout" onEnter={logOut}/>
         <Route path="/" component={Layout} onEnter={checkForUserSession}>
           <IndexRoute component={Homepage}/>
           <Route path="login" component={Login}/>
