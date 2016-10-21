9e1ffe54b14ff9e6860bb56df787988ed8913ee4
Ramsay Lanier <rlanier@NovettaminsMBP2.fios-router.home>
2016-10-21 10:52:32 -0400

    add ui reducer to combine reducers

--- src/apollo.js
+++ src/apollo.js
@@ -3,12 +3,14 @@ import ApolloClient from 'apollo-client'
 import { routerReducer } from 'react-router-redux'
 
 import userReducer from './state/reducers/userReducer'
+import uiReducer from './state/reducers/uiReducer'
 
 export const client = new ApolloClient()
 
 export const store = createStore(
   combineReducers({
     user: userReducer,
+    ui: uiReducer,
     apollo: client.reducer(),
     routing: routerReducer
   }),
